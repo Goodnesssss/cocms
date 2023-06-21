9304fd1f1cf5c1f5f7a46fb108db1487d616f2da
Goodnesssss <2209281294@qq.com>
2023-06-21 23:10:46 +0800

    submit

---
+++ cocms_server/src/main/java/com/nchu16_07/cocms_server/CocmsServerApplication.java
@@ -0,0 +1,13 @@
+package com.nchu16_07.cocms_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CocmsServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CocmsServerApplication.class, args);
+    }
+
+}
---
+++ cocms_server/src/test/java/com/nchu16_07/cocms_server/CocmsServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nchu16_07.cocms_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CocmsServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
